87d359bcd659ba4787ec6f8946f60e91642c1558
Jazmin Feijoo <jazminfeijoo@Jazmins-MBP.lan>
2021-10-30 18:52:17 -0400

    problem 3 started

---
+++ TW-3-MaxPresentationsArrays.js
@@ -0,0 +1,25 @@
+// A schedule has just been released for an upcoming tech conference. 
+// The schedule provides the start and end times of each of the presentations. 
+// Once a presentation has begun, no one can enter or leave the room. 
+// It takes no time to go from one presentation to another. 
+// Determine the maximum number of presentations that can be attended by one person.
+
+// Complete the function maxPresentations in the editor below.
+
+// maxPresentations has the following parameter(s)：
+//   scheduleStart[n]：the start times of presentation i
+//   scheduleEnd[n]：the end times of presentation i
+
+// Returns：
+//   int：the maximum number of presentations that can be attended by one person
+function maxPresentations(startSched, endSched){
+    let final = 0
+    
+    console.log(startSched, endSched)
+
+    return final
+}
+
+let start1 = [1, 1, 2, 3]
+let end1 = [2, 3, 3, 4]
+maxPresentations(start1, end1) //expected output : 3
