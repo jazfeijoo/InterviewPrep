81f9f602a276736a325e454915af02ef399ec926
Jazmin Feijoo <jazminfeijoo@Jazmins-MacBook-Pro.local>
2021-11-20 15:20:53 -0500

    Generate Parenthesis: almost solved!..

--- GGL-1-Generate-Parenthesis.js
+++ GGL-1-Generate-Parenthesis.js
@@ -1,73 +1,56 @@
 // Problem: Given an integer N representing the number of pairs of parentheses, 
 // the task is to generate all combinations of well-formed(balanced) parentheses.
 
+
 function allParenthesis(n){ //d=depth/layers
     let final = []
-    let open = n
-    let close = n
-    let str=''
-    function allParHelper(n, open, close, final, str){
-        if (open === 0 && close === 0){
-            final.push(str)
-            console.log(final)
-            return final
-        } else {
-            if (open === n && close === n){
-                allParHelper(n,open--,close,final,'(')
-            } else {
-                //OPEN OR CLOSE are not at beginning & not at end...
-                if (0 < open && close <= open){
-                    allParHelper(n, open--, close, final, str.concat('('))
-                } else {
-                    allParHelper(n, open, close--,str.concat(')'))
-                }
-            }
-        }
-    }
-    allParHelper(n, open, close, final, str)
-
-
+    allParHelper(n, 0, 0, "",final)
+    console.log('FINAL SOLVED BY J: ', final)
+    return final 
 }
 
+function allParHelper(n, open, close, str, final){
+    if (close === n){
+        final.push(str)
+        return
+    } else {
+            if(open > close ){ //add '(' IF VALID for each n///
+                str = str.concat(')')
+                allParHelper(n, open, close + 1, str,final)
+               // str.slice(0,-1)
+            }
+            if(open < n){ //add ')' IF VALID for each n
+                str = str.concat('(')
+                allParHelper(n, open + 1, close, str,final)
+               // str.slice(0,-1)
+            }
+    }
+}
 
-//     for (let i=1; i<=n;i++){
-//         const par = '()'
-//         if(str.length === (n*2)){
-//             final.push(str)
-//         } else {
-//             if (open === n && close === n){
-//                 str = '('
-//                 open--
-//                 allParenthesis(n, final, str, open, close)
-//             }
-//             else {
-//                 //len= 1-5
-//                 if( 1 <= open){
-//                     str = str.concat('(')
-//                     open --
-//                     allParenthesis(n, final, str, open, close)
-//                 }
-//                 if ( 1 <= close){
-//                     str = str.concat(')')
-//                     close --
-//                     allParenthesis(n, final, str, open, close)
-//                 }
-//             }
+allParenthesis(2) // OUTPUT: ((())) and (()()) and (())() and ()(()) and ()()()
+
+
+// var generateParenthesis = function(n) {
+//     let result = [];
+
+//     _generateParenthesis("", n, 0, 0, result);
+//     console.log('NEW FINAL: ',result)
+//     return result;
+// };
+
+// var _generateParenthesis = function(current, n, left, right, result){
+//     if( right === n ) {
+//         result.push(current);
+//         return;
+//     } else {
+//         if( left < n ) {
+//            _generateParenthesis(current + '(', n, left + 1, right, result);
+//         }
+
+//         if( left > right) {
+//             _generateParenthesis(current + ')', n, left, right + 1, result);
 //         }
 //     }
-//     console.log('FINAL: ', final)
-//     return final
 // }
 
-// function outerParenth(n){
-//     let string = ''
-//     for (let i=0; i<n; i++){
-//         string = string.concat('()')
-//     }
-//     return string
-// }
-// outerParenth(3)
-//allParenthesis(1,0, 1,[]) //OUTPUT: ()
-allParenthesis(3) // OUTPUT: ((())) and (()()) and (())() and ()(()) and ()()()
-
-let string1 = '()'
+// generateParenthesis(3)
