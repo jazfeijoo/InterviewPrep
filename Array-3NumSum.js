826f8f16dada48b343a2da6696f3858990da0130
Jazmin Feijoo <jazminfeijoo@Jazmins-MBP.lan>
2021-12-07 17:09:44 -0500

    Array-3NumSUm: Solved

--- Array-3NumSum.js
+++ Array-3NumSum.js
@@ -4,18 +4,28 @@
 //EX OUTPUT: [-8,2,6], [-8,3,5],[-6,1,5]
 
 function TripletNumSum(arr, target){
-    let final = []
     arr.sort((a,b) => a - b)
-    let lowIdx = 0
-    let highIdx = arr.length -1
-    let currIdx = 0
-
-    for (let i=0; i<arr.length; i++){
-        let sumList = [arr[i]]
-        let sumTarg = target - arr[i]
-        
+    let final = []
+    for (let i = 0; i < arr.length - 2; i++){
+        let next = i+1
+        let end = arr.length - 1
+        let sum = target - arr[i]
+        while (next < end){
+            if ( sum === arr[next] + arr[end]){
+                final.push([arr[i], arr[next], arr[end]])
+                next++
+                end--
+            }
+            else if (arr[next] + arr[end] < sum){
+                next ++
+            } else { //sum is less than target, need 
+                end --
+            }
+            
+        }
     }
-
+    console.log('FINAL: ',final)
+    return final
 }
 
 TripletNumSum([12,3,1,2,-6,5,-8,6],0)
