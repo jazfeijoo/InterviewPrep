9ff1ea64105e53afd965dbdc36a09205e58a48fd
Jazmin Feijoo <jazminfeijoo@Jazmins-MBP.lan>
2021-12-07 17:26:29 -0500

    CoderPro-P9-FindNonDuplicateNum: solution 1 completed; solution 2 started...

--- CoderPro-P9-FindNonDuplicateNum.js
+++ CoderPro-P9-FindNonDuplicateNum.js
@@ -1,3 +1,20 @@
 // FIND NON-DUPLICATE NUM IN AN ARRAY THAT HAS ALL REPEATING/DUPLICATE NUMS EXCEPT FOR 1 INTEGER...
 // Solution 1: Hash Map
-// SOlution 2: XOR() method
+// Solution 2: XOR() method
+let arr1 = [4, 3, 2, 4, 7, 3, 2, 2]
+
+function findNonDup(arr){
+    let map = {}
+    for(let i=0; i< arr.length-1;i++){
+        map[arr[i]] ? map[arr[i]] += 1 : map[arr[i]] = 1 
+    }
+    console.log(map)
+    for (num in map){
+        if (map[num] === 1) {
+            console.log(num)
+            return num
+        }
+    }
+}
+
+findNonDup(arr1)
